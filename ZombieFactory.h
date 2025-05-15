11d363c7c87ddbafc2043aa7a90d525c2e8249a0
Shahmeer-Atif <shahmeeratif49@gmail.com>
2025-05-15 17:09:45 +0500

    Update ZombieFactory.h

--- ZombieFactory.h
+++ ZombieFactory.h
@@ -7,8 +7,6 @@
 #include"Global.h"
 #include"Animation.h"
 #include"Zombies.h"
-//Group Member 1: Shahmeer Atif 23i-0711 CS-G
-//Group Member 2: Irfan Khalid  23i-2092 CS-G
 using namespace std;
 using namespace sf;
 class Zombie_Factory {
